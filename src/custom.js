9db1d6e30ee4a12338f387a898bfab3542cfb76b
Pinku rathour <pinkurathour2016@gmail.com>
2024-10-07 12:32:36 +0530

    add message me

--- src/custom.js
+++ src/custom.js
@@ -3,3 +3,35 @@ function toggleMenu() {
   const menu = document.getElementById("mobileMenu");
   menu.classList.toggle("hidden");
 }
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: false, 
+  dots: false,
+  smartSpeed: 500, 
+  autoplaySpeed: 800,
+  navSpeed: 800,
+  navText: [
+    '<span class="iconify" data-icon="oui:arrow-left"></span>',
+    '<span class="iconify" data-icon="oui:arrow-right"></span>',
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 3,
+    },
+    1180: {
+      items: 4,
+    },
+  },
+});
+
